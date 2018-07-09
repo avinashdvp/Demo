dafed8e7796d673eadf458a6992d86491c68c285
avinashdvp <40980562+avinashdvp@users.noreply.github.com>
2018-07-09 22:01:30 +0530

    Add files via upload

---
+++ lastdig.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Lastdigit
+{
+public static void main(String args[])
+{
+int a,b;
+Scanner c=new Scanner(System.in);
+System.out.println("enter any number");
+a=c.nextInt();
+b=a%10;
+System.out.println("last digit of given number is" +b);
+}
+}
+
+
