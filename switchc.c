277ef0a26bda2e01b731729c532ac1ed18b81145
avinashdvp <40980562+avinashdvp@users.noreply.github.com>
2018-07-09 22:21:24 +0530

    Add files via upload

---
+++ switchc.c
@@ -0,0 +1,25 @@
+import java.util.*;
+class Switch
+{
+public static void main(String args[])
+{
+int a,b,c,d,e;
+Scanner s=new Scanner(System.in)
+System.out.println("enter any number");
+a=s.nextInt();
+switch(a):
+{
+case 1:
+System.out.println("1");
+break;
+case 2:
+System.out.println("2");
+break;
+case 3:
+System.out.println("3");
+break;
+default:
+System.out.println("entered value is more than 3");
+}
+}
+}
