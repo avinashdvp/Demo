a7f36858ec6a4f7d0f8884537f9be57c49628094
avinashdvp <40980562+avinashdvp@users.noreply.github.com>
2018-07-09 22:27:24 +0530

    Add files via upload

---
+++ Basic.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Basic
+{
+//execution entry point
+public static void main(String args[])
+{
+int a,b,c;
+//output statement
+System.out.println("enter any two numbers");
+Scanner se=new Scanner(System.in);
+a=se.nextInt();
+b=se.nextInt();
+c=a+b;
+System.out.println("addition = "+c);
+}
+}
+ 
