1f008589553e0f2624ff2375fa48d9f94b355207
avinashdvp <40980562+avinashdvp@users.noreply.github.com>
2018-07-09 23:08:07 +0530

    Add files via upload

---
+++ biggest.c
@@ -0,0 +1,28 @@
+//biggest number
+#include<stdio.h>
+void main()
+{
+	int a[100],n,i,j,temp;
+	puts("how many numbers ?");
+	scanf("%d",&n);
+	printf ("enter the numbers");
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	for(i=0;i<n;i++)
+	{
+		if(a[i]>a[i+1])
+		{
+			temp=a[i];
+			a[i]=a[i+1];
+			
+			a[i+1]=temp;
+		}
+		if(i==n-1)
+		{
+		printf("biggest number is %d",a[n]);
+		}
+	}
+	
+}
