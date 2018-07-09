5ee0913273481510a39ddfd2478b7ddeaa925eeb
avinashdvp <40980562+avinashdvp@users.noreply.github.com>
2018-07-10 01:13:31 +0530

    Add files via upload

---
+++ prob.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+	int rn1,rn2,rn3,rn4;
+	puts("the first five student roll numbers are");
+	rn1=1;
+	rn2=2;
+	rn3=3;
+	rn4=4;
+	printf("%d\n",rn1);
+	printf("%d\n",rn2);
+	printf("%d\n",rn3);
+	printf("%d\n",rn4);
+}
+
