ca60dba362c29d100e87ad19809a298995c2f3dc
avinashdvp <40980562+avinashdvp@users.noreply.github.com>
2018-07-09 22:05:15 +0530

    Add files via upload

---
+++ votetest.c
@@ -0,0 +1,16 @@
+//eligibility test 
+#include<stdio.h>
+void main()
+{
+int age;
+puts("enter the age");
+scanf("%d",&age);
+if(age>18)
+{
+puts("eligible for voting");
+}
+else
+{
+puts("not eligible for voting");
+}
+}
