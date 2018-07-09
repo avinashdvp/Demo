224df666f37b5caa88019b72f96d5124be446522
avinashdvp <40980562+avinashdvp@users.noreply.github.com>
2018-07-09 22:06:34 +0530

    Add files via upload

---
+++ even.c
@@ -0,0 +1,18 @@
+//testing a given n umber is even or odd
+#include<stdio.h>
+void main()
+{
+	int num;
+	puts("enter the number to test");
+	scanf("%d",&num);
+	if(num%2==0)
+	{
+		puts("entered number is even");
+	}
+	else
+	{
+		puts("entered number is odd");
+		
+	}
+}
+
