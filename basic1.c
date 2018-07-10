2da1dd87775543988c13b9d2205802dd9645ae75
avinashdvp <40980562+avinashdvp@users.noreply.github.com>
2018-07-10 11:32:13 +0530

    Add files via upload

---
+++ basic1.c
@@ -0,0 +1,14 @@
+//printing #include<stdio.h>
+void main()
+{
+	puts("trial");
+	int x;
+	char z;
+	float y;
+    x=10;
+    y=20.56;
+    z='a';
+printf("%d",x);
+printf("%c",z);
+printf("%f",y);
+}
