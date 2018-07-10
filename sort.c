e49f35ee89cabb88a43fde844d4d741e0de76e65
avinashdvp <40980562+avinashdvp@users.noreply.github.com>
2018-07-10 09:09:54 +0530

    Add files via upload

---
+++ sort.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+void main()
+{
+	int a[100],b,n,j,i,temp;
+	puts("enter numbers to be sorted");
+	for(i=0;i<5;i++)
+		{
+		scanf("%d",&a[i]);
+		}
+		for(j=0;j<5;j++)
+		{
+		for(i=j;i<5;i++)
+		{
+			if(a[i]>a[i+1])
+			{
+				temp=a[i];
+				a[i]=a[i+1];
+				a[i+1]=temp;
+			}
+			if(i==4)
+			{
+				temp=a[i];
+				a[i]=a[i+1];
+				a[i+1]=temp;
+				
+		printf("%d",a[j]);
+			}
+		}
+	}
+}
+
+
