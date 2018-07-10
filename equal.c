429807b070e5b993347141c8e36f16e4d829bd61
avinashdvp <40980562+avinashdvp@users.noreply.github.com>
2018-07-10 11:37:10 +0530

    Add files via upload

---
+++ equal.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+	int a,b;
+	puts("enter any two numbers to check they are equal are not");
+	scanf("%d %d",&a,&b);
+	if(a==b)
+	{
+		printf("%d and  %d are equal",a,b);
+	}
+	else
+	printf("%d and %d are not equal",a,b);
+	
+}
